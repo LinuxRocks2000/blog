4c6cb5768b60ecbc6445e047df69ac3bcc20e743
The Buzzard <plupy@awesome.local>
2022-02-27 10:37:50 -0500

    Post a recipe, and now footnotes work! I've gotta start adding them in.

--- _posts/2021-07-26-hello-world.md
+++ _posts/2021-07-26-hello-world.md
@@ -12,7 +12,7 @@ Seriously, as this is a first blog post, I must make an important point clear:
 some of my posts may joke in socially incorrect ways - racism, religion, and politics, to name a few. These are intended to be funny and I will try to stay fair. If you are offended, I'm sorry. You can't expect the internet to always be a fairyland, and if you want something to be offended by, look anywhere on the internet. Getting your pannies in a wad will just waste my time.
 
 
-Blogging is undeniably fun, but the routines can be hard - I won't be posting weekly, and sometimes I might post daily.<collapsible-footnote>Hi</collapsible-footnote>  
+Blogging is undeniably fun, but the routines can be hard - I won't be posting weekly, and sometimes I might post daily.<collapsible-footnote citationName="[1]">This, quite obviously, is a footnote. *Italics with Markdown?* don't work, but <i>Italics with HTML</i> do. Dang, gotta remember that.</collapsible-footnote>  
 
 
 ### I hope you like my site!
--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
 class FootNote extends HTMLElement{
     constructor(){
         super();
-        this.attachShadow({mode:"open"});
-        var superscript = document.createElement("sup");
-        superscript.innerHTML = "[citation]";
-        this.shadowRoot.appendChild(superscript);
+        this.superscript = document.createElement("sup");
+        this.data = document.createElement("div");
+        this.superscript.onclick = () => {
+            this.supClicked();
+        };
+        this.superscript.classList.add("citationSup");
+        this.data.style.display = "none";
+        this.data.style.position = "absolute";
+        this.data.style.backgroundColor = "wheat";
+        this.data.style.padding = "5px";
+        this.data.style.maxWidth = "200px";
+        this.data.style.fontSize = "0.75em";
+        this.data.innerHTML = this.innerHTML;
+        this.innerText = "";
+        this.appendChild(this.data);
+        this.appendChild(this.superscript);
+    }
+    static get observedAttributes(){
+        return ["citationname"];
+    }
+    attributeChangedCallback(attr, oldval, val){
+        switch(attr){
+            case "citationname":
+                this.citationName = val;
+                this.superscript.innerHTML = val;
+            break;
+        }
+    }
+    /*set citationname(data){
+        alert(data);
+    }*/
+    supClicked(){
+        var rect = this.superscript.getBoundingClientRect();
+        this.data.style.top = rect.y + rect.height + document.documentElement.scrollPosition + "px";
+        this.data.style.left = rect.x + rect.width + "px";
+        if (this.data.style.display == "block"){
+            this.data.style.display = "none";
+        }
+        else{
+            this.data.style.display = "block";
+        }
     }
     connectedCallback(){
         console.log(this);
